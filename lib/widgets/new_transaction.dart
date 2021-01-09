c880f4ed05c3d24591fe013a163dd37184030df8
Park Soravee <park11102001@gmail.com>
2021-01-09 14:17:02 +0700

    created addNewTransaction method and passed in to FlatButton onPressed

---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  final Function addTx;
+
+  NewTransaction(this.addTx);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Title',
+              ),
+              controller: titleController,
+              // onChanged: (val) {
+              //   titleInput = val;
+              // },
+            ),
+            TextField(
+              decoration: InputDecoration(
+                //hintText: '0.00',
+                labelText: 'Amount',
+              ),
+              controller: amountController,
+              // onChanged: (val) => amountInput = val,
+            ),
+            FlatButton(
+              onPressed: () {
+                print(titleController.text);
+                print(amountController.text);
+                addTx(
+                  titleController.text,
+                  double.parse(amountController.text),
+                );
+              },
+              textColor: Colors.purple,
+              child: Text('Add Transaction'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
