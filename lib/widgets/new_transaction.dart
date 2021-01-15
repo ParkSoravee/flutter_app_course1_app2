bbcbb900c97da73cf23eb65c1ad2ac122e730b56
Park Soravee <park11102001@gmail.com>
2021-01-15 15:48:54 +0700

    fixed error when add transaction without amount.

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -16,10 +16,13 @@ class _NewTransactionState extends State<NewTransaction> {
   DateTime _selectedDates;
 
   void _submitData() {
+    if (_amountController.text.isEmpty) {
+      return;
+    }
     final enteredTitle = _titleController.text;
     final enteredAmount = double.parse(_amountController.text);
 
-    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+    if (enteredTitle.isEmpty || enteredAmount <= 0 || _selectedDates == null) {
       return;
     }
 
